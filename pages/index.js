70a0e5716230dca1beba1a21a7da99e3185344b3
taibouzu <taibouzu@example.com>
2021-05-30 01:26:38 +0900

    add link tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className="container">
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Learn <Link href="posts/first-post"><a>Next.js!</a></Link>
         </h1>
 
         <p className="description">
